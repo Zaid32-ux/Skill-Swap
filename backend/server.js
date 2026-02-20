c0a515e1b78b1b7d8c7eb6f8680b3b08a16d3abc
Zaid32-ux <Zgour5071@gmail.com>
2026-02-20 16:17:24 +0530

    initialize express server and socket.io

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,16 @@
 import app from "./app.js";
+import { Server } from "socket.io";
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running at port ${process.env.PORT}`);
+const PORT = process.env.PORT || 3001;
+   const server =app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
 });
+
+
+const io = new Server(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "*", // in production put your frontend URL
+  },
+});
+
